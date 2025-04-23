9970a6be5d8052a3c6ab68eaacd39c0ed2a96b4a
joseluisromero <manabis.rum@hotmail.com>
2025-04-23 00:32:47 -0500

    chekea si un numero es guay

---
+++ src/main/java/com/code/JlrMsaCodeTestApplication.java
@@ -0,0 +1,25 @@
+package com.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlrMsaCodeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlrMsaCodeTestApplication.class, args);
+		verifyNumericIsGuay(10);
+	}
+
+	public static void verifyNumericIsGuay(int n) {
+		int sum = 0;
+		for (int i = 1; i < n / 2; i++) {
+			sum += i;
+		}
+		if (sum == n) {
+			System.out.println("El número " + n + " es guay");
+		} else {
+			System.out.println("El número " + n + " no es guay");
+		}
+	}
+}
